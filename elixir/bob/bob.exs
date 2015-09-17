eb3f6f6ae91f66ddc915f71cf164d94a19ef651f
BryanaKnight <brknig11@gmail.com>
2015-09-16 20:38:16 -0400

    elixir bob

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -1,7 +1,38 @@
 defmodule Teenager do
+
   def hey(input) do
     cond do
-
+        no_words(input) ->
+          "Fine. Be that way!"
+        all_numbers(input) ->
+          "Whatever."
+        question(input) ->
+          "Sure."
+        yelling(input) or russian(input) ->
+          "Whoa, chill out!"
+        true ->
+          "Whatever."
     end
   end
+
+  def no_words(phrase) do
+    String.length(String.strip(phrase)) == 0
+  end
+
+  def all_numbers(phrase) do
+    Enum.all?(String.split(phrase, ","), fn(x) -> String.contains?("1234567890", String.strip(x)) end)
+  end
+
+  def question(phrase) do
+    String.ends_with?(phrase, "?")
+  end
+
+  def yelling(phrase) do
+    String.upcase(phrase) == phrase
+  end
+  
+  def russian(phrase) do
+    Enum.any?(String.split(phrase, ","), fn(x) -> String.contains?("УХОДИТЬ", String.strip(x)) end)
+  end
+
 end
